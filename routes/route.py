bd3021fb204f6ed760fb91fda988fa010f7cb34f
aradhyasakalley <sakalleyaradhya@gmail.com>
2024-03-07 19:33:51 +0530

    Updated uri

--- routes/route.py
+++ routes/route.py
@@ -3,9 +3,23 @@ from models.todos import Todo
 from config.database import collection_name
 from schema.schemas import list_serial
 from bson import ObjectId
-
+import joblib
+from models.CarUser import CarUser
 router = APIRouter()
+joblib_in = open("car-recommender.joblib","rb")
+model=joblib.load(joblib_in)
 
+@router.post('/car/predict')
+def predict_car_type(data:CarUser):
+    data = data.dict()
+    age=data['age']
+    gender=data['gender']
+
+    prediction = model.predict([[age, gender]])
+    
+    return {
+        'prediction': prediction[0]
+    }
 @router.get("/")
 async def get_todos():
     todos = list_serial(collection_name.find())
